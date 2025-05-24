ea2d2073ac3678794bc3f5baaf7da320b32e6f14
Rajendra <bkrajendra@gmail.com>
2025-05-24 19:51:41 +0530

    Updated app id and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rajendrakhope',
-  appName: 'mobileradio',
+  appId: 'com.rajendrakhope.mobileradio',
+  appName: 'MobileRadio',
   webDir: 'www'
 };
 
